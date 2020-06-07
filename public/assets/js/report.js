d16e7184de09255b14740c7e86b3776b10d10746
jollyrgr83 <jasongmangin@gmail.com>
2020-06-07 09:43:30 -0600

    completed rendering of top 3 and all reports

--- public/assets/js/report.js
+++ public/assets/js/report.js
@@ -9,9 +9,9 @@ $(() => {
 
     $(document).on("change", "#year-select", event => getAll());
 
-    $(document).on("change", "#report-select", event => renderEventMenuAll());
+    $(document).on("change", "#report-select", event => renderTierMenu());
 
-    $(document).on("change", "#dynamic-select-tier", event => renderEventMenuTier());
+    $(document).on("change", "#tier-select", event => renderEventMenuTier());
 
     $(document).on("change", "#dynamic-select-org", event => renderScores());
 
@@ -21,106 +21,30 @@ $(() => {
         $.get("/api/report/all/" + parseInt($("#year-select").val()), (data) => {
             obj = JSON.parse(JSON.stringify(data));
             console.log("obj", obj);
-            renderEventMenuAll();
+            renderTierMenu();
         });
     }
 
-    function renderEventMenuAll() {
+    function renderTierMenu() {
+        $("#tier-select").empty();
         $("#event-select").empty();
         $("#dynamic-menu").empty();
-        if ($("#report-select").val() === "2") {
-            // render dynamic menu
-            $("#dynamic-menu").empty();
-            let menuArray = [];
-            let tier_id_array = Object.keys(obj.tiers);
-            for (let i = 0; i < tier_id_array.length; i++) {
-                let tier_id = parseInt(tier_id_array[i]);
-                menuArray.push({id: tier_id, text: `${obj.tier_ref[tier_id].name}`});
-            }
-            var pTitleEl = $("<p>");
-            pTitleEl.text("Select Competition Tier");
-            pTitleEl.attr("class", "item-title mx-auto");
-            $("#dynamic-menu").append(pTitleEl);
-            var selectEl = $("<select>");
-            selectEl.attr("id", "dynamic-select-tier");
-            for (let i = 0; i < menuArray.length; i++) {
-                var optionEl = $("<option>");
-                optionEl.text(menuArray[i].text);
-                optionEl.attr("value", menuArray[i].id);
-                selectEl.append(optionEl);
-            }
-            $("#dynamic-menu").append(selectEl);
-            // render event selections
-            let optionArray = [];
-            let tier_id = menuArray[0].id;
-            let event_id_array = Object.keys(obj.tiers[tier_id]);
-            for (let i = 0; i < event_id_array.length; i++) {
-                if (isNaN(parseInt(event_id_array[i]))) {
-                    var event_id = event_id_array[i];
-                } else {
-                    var event_id = parseInt(event_id_array[i]);
-                }
-                optionArray.push({id: `${tier_id}-${event_id}`, text: `${obj.tier_ref[tier_id].name} - ${obj.event_ref[event_id]}`});
-            }
-            for (let i = 0; i < optionArray.length; i++) {
-                var optionEl = $("<option>");
-                optionEl.text(optionArray[i].text);
-                optionEl.attr("value", optionArray[i].id);
-                $("#event-select").append(optionEl);
-            }
-        } else {
-            // retrieve all competitors information (View Top 3 and View All Results)
-            if ($("#report-select").val() === "3") {
-                // render dynamic menu
-                $("#dynamic-menu").empty();
-                let menuArray = [];
-                let org_id_array = Object.keys(obj.org_ref);
-                for (let i = 0; i < org_id_array.length; i++) {
-                    let org_id = parseInt(org_id_array[i]);
-                    menuArray.push({id: org_id, text: `${obj.org_ref[org_id].name}`});
-                }
-                var pTitleEl = $("<p>");
-                pTitleEl.text("Select Organization");
-                pTitleEl.attr("class", "item-title mx-auto");
-                $("#dynamic-menu").append(pTitleEl);
-                var selectEl = $("<select>");
-                selectEl.attr("id", "dynamic-select-org");
-                for (let i = 0; i < menuArray.length; i++) {
-                    var optionEl = $("<option>");
-                    optionEl.text(menuArray[i].text);
-                    optionEl.attr("value", menuArray[i].id);
-                    selectEl.append(optionEl);
-                }
-                $("#dynamic-menu").append(selectEl);
-            }
-            let optionArray = [];
-            let tier_id_array = Object.keys(obj.tiers);
-            for (let i = 0; i < tier_id_array.length; i++) {
-                let tier_id = parseInt(tier_id_array[i]);
-                let event_id_array = Object.keys(obj.tiers[tier_id]);
-                for (let j = 0; j < event_id_array.length; j++) {
-                    if (isNaN(parseInt(event_id_array[j]))) {
-                        var event_id = event_id_array[j];
-                    } else {
-                        var event_id = parseInt(event_id_array[j]);
-                    }
-                    optionArray.push({id: `${tier_id}-${event_id}`, text: `${obj.tier_ref[tier_id].name} - ${obj.event_ref[event_id]}`});
-                }
-            }
-            for (let i = 0; i < optionArray.length; i++) {
-                var optionEl = $("<option>");
-                optionEl.text(optionArray[i].text);
-                optionEl.attr("value", optionArray[i].id);
-                $("#event-select").append(optionEl);
-            }
-            renderScores()
+        // render tier-select menu
+        let menuArray = [];
+        let tier_id_array = Object.keys(obj.tiers);
+        for (let i = 0; i < tier_id_array.length; i++) {
+            let tier_id = parseInt(tier_id_array[i]);
+            menuArray.push({id: tier_id, text: `${obj.tier_ref[tier_id].name}`});
         }
-    }
-
-    function renderEventMenuTier() {
-        $("#event-select").empty();
+        for (let i = 0; i < menuArray.length; i++) {
+            var optionEl = $("<option>");
+            optionEl.text(menuArray[i].text);
+            optionEl.attr("value", menuArray[i].id);
+            $("#tier-select").append(optionEl);
+        }
+        // initialize event-select menu
         let optionArray = [];
-        let tier_id = parseInt($("#dynamic-select-tier").val());
+        let tier_id = menuArray[0].id;
         let event_id_array = Object.keys(obj.tiers[tier_id]);
         for (let i = 0; i < event_id_array.length; i++) {
             if (isNaN(parseInt(event_id_array[i]))) {
@@ -136,9 +60,80 @@ $(() => {
             optionEl.attr("value", optionArray[i].id);
             $("#event-select").append(optionEl);
         }
+        renderScores();
+    }
+
+    function renderEventMenuTier() {
+        $("#event-select").empty();
+        let optionArray = [];
+        let tier_id = parseInt($("#tier-select").val());
+        let event_id_array = Object.keys(obj.tiers[tier_id]);
+        for (let i = 0; i < event_id_array.length; i++) {
+            if (isNaN(parseInt(event_id_array[i]))) {
+                var event_id = event_id_array[i];
+            } else {
+                var event_id = parseInt(event_id_array[i]);
+            }
+            optionArray.push({id: `${tier_id}-${event_id}`, text: `${obj.tier_ref[tier_id].name} - ${obj.event_ref[event_id]}`});
+        }
+        for (let i = 0; i < optionArray.length; i++) {
+            var optionEl = $("<option>");
+            optionEl.text(optionArray[i].text);
+            optionEl.attr("value", optionArray[i].id);
+            $("#event-select").append(optionEl);
+        }
+        renderScores();
     }
 
     function renderScores() {
-
+        $("#dynamic").empty();
+        var tier_id = parseInt($("#event-select").val().split("-")[0]);
+        var event_id = isNaN(parseInt($("#event-select").val().split("-")[1])) ? $("#event-select").val().split("-")[1] : parseInt($("#event-select").val().split("-")[1]);
+        // render event title
+        var pTitleEl = $("<p>");
+        pTitleEl.attr("class", "mini-title mx auto");
+        pTitleEl.text(obj.event_ref[event_id]);
+        $("#dynamic").append(pTitleEl);
+        if ($("#report-select").val() === "0") {
+            var x = 3;
+        } else if ($("#report-select").val() === "1") {
+            var x = obj.tiers[tier_id][event_id].length;
+        }
+        for (let i = 0; i < x; i++) {
+            // retrieve competitor information
+            let competitor_id = obj.tiers[tier_id][event_id][i].competitor_id;
+            // render html elements
+            var divEl = $("<div>");
+            divEl.attr("class", "result-container mx-auto");
+            var pRankEl = $("<p>");
+            pRankEl.attr("class", "mini-title mx-auto");
+            pRankEl.text(`Place: ${i + 1}`);
+            var pCompNumberEl = $("<p>");
+            pCompNumberEl.attr("class", "item-title mx-auto");
+            pCompNumberEl.text(obj.comp_ref[competitor_id].comp_number);
+            var pNameEl = $("<p>");
+            pNameEl.attr("class", "item-title mx-auto");
+            if (obj.tier_ref[tier_id].team === 0) {
+                pNameEl.text(`${obj.comp_ref[competitor_id].first_name} ${obj.comp_ref[competitor_id].last_name}`);
+            } else {
+                pNameEl.text(`${obj.comp_ref[competitor_id].team_name}: ${obj.comp_ref[competitor_id].group_names}`);
+            }
+            var pOrgEl = $("<p>");
+            pOrgEl.attr("class", "item-title mx-auto");
+            pOrgEl.text(obj.comp_ref[competitor_id].org_name);
+            var pScoreEl = $("<p>");
+            pScoreEl.attr("class", "item-title mx-auto");
+            pScoreEl.text(obj.tiers[tier_id][event_id][i].score);
+            var pTimeEl = $("<p>");
+            pTimeEl.attr("class", "item-title mx-auto");
+            pTimeEl.text(`${Math.floor(obj.tiers[tier_id][event_id][i].total_seconds / 60)}:${(obj.tiers[tier_id][event_id][i].total_seconds % 60).toFixed(2)}`);
+            divEl.append(pRankEl);
+            divEl.append(pCompNumberEl);
+            divEl.append(pNameEl);
+            divEl.append(pOrgEl);
+            divEl.append(pScoreEl);
+            divEl.append(pTimeEl);
+            $("#dynamic").append(divEl);
+        }
     }
 });
